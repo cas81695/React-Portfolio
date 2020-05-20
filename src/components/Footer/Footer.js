4d03efe90a8dbc2cd767c0259cf18d230cf85c84
Christian Sanchez <cas81695@gmail.com>
2020-05-19 23:50:20 -0400

    added footer and header

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer(){
+  return(
+<>
+<footer className="center">
+
+  <div className="sectionMargin">
+   
+    <p id="copyright">Copyright &copy; Stephanie Lebby 
+    <br></br>
+    <span className="footerIcons">
+      <a href="https://github.com/cas81695"><i className="fa fa-github icons"></i></a><a href="https://www.linkedin.com/in/christian-sanchez-181467103"><i className="fa fa-linkedin icons"></i></a> 
+    </span>
+    </p>
+  </div>
+  
+</footer>
+</>
+)
+}
+export default Footer;
+ 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link} from "react-router-dom";
+
+import "./Navbar.css";
+
+function Navbar(){
+ 
+const handleToggle=  () => {
+    var x = document.getElementById("navFixed");
+    if (x.className.indexOf("show") === -1 && x.className.indexOf("hide-small") === 0) {
+        x.className = x.className.replace("hide-small", " show");
+    } else {
+        x.className = x.className.replace(" show", "hide-small");
+    }
+}
+  return(
+    <>
+    <div className="nav-top">
+      <div id="navbar">
+        <button id ="navLink" className="button hide-md-lg" onClick={handleToggle} title="Toggle Navigation Menu">
+          <i className="fa fa-bars"></i>
+        </button>
+        <Link to="/" className="navbar-item button">HOME</Link>
+        <Link to="/about" className="navbar-item button hide-small"><i className="fa fa-female"></i> ABOUT</Link>
+        <Link to="/portfolio" className="navbar-item button hide-small"><i className="fa fa-th"></i> PORTFOLIO</Link>
+        <Link to="/contact" className="navbar-item button hide-small"><i className="fa fa-address-card"></i> CONTACT</Link>
+      </div>
+
+      {/* <!-- Navbar small--> */}
+      <div id="navFixed" className="hide-small hide-large hide-medium" onClick={handleToggle}>
+        <Link to="/about" className="navbar-item small button">ABOUT</Link>
+        <Link to="/portfolio" className="navbar-item small button" >PORTFOLIO</Link>
+        <Link to="/contact" className="navbar-item  small button" >CONTACT</Link> 
+      </div>
+    </div>
+    </>
+
+  )
+}
+
+export default Navbar;
