47d78790e7a0c7479beb82adc60d1324293fec39
Christian Sanchez <cas81695@gmail.com>
2020-05-19 23:25:39 -0400

    added home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../index.css";
+import MainSectionImage from "../../components/SectionImage/MainSectionImage"
+
+
+function Home(){
+  return (
+    <>
+    <MainSectionImage/>
+    </>
+  )
+}
+
+export default Home;
